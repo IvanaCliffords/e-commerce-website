e8e42496e4d87f79af73b916a552534dc956cff6
IvanaCliffords <ivana.cliffords>
2022-04-30 15:37:08 -0500

    make it better

--- frontend/src/layout/Footer.jsx
+++ frontend/src/layout/Footer.jsx
@@ -4,8 +4,11 @@ function Footer() {
   return (
     <div className="footer">
       <h3>
-        All right reserved &copy;free
-        <span style={{ color: "#6bc7ed", fontWeight: "700" }}>water </span>
+        All right reserved{" "}
+        <a href="https://www.freewater.io/" target="_blank" className="footer-link">
+          free
+          <span style={{ color: "#6bc7ed", fontWeight: "700" }}>water </span>
+        </a>
       </h3>
     </div>
   );
